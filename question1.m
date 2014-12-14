e8054f0093db3143c87e4199e0a46f4499cb40b7
Ryan Misura <misura@wisc.edu>
2014-12-14 10:58:41 -0600

    main file changes

--- question1.m
+++ question1.m
@@ -1,15 +1,15 @@
 [train,tune,test,dataDim] = getFederalistData();
 features = 1:70;
-mu = [0 .001 .01 .1 1 10 100];
+mu = [0 .001 .01 .1 1 10 100 100000];
 
 for i=1:size(mu,2)
     [w,gam,objective] = separateQP(train,features,mu(i));
     [correct_Train,wrong_Train] = discrim(w,gam,train,features);
     [correct_Tune,wrong_Tune] = discrim(w,gam,tune,features);
-    fprintf('%%%%%%%%%%%%%% MU = %d\n',mu(i));
-    fprintf('Optimal Objective: %d\n',objective);
-    fprintf('Gamma: %d\n',gam);
-    fprintf('2-Norm of w: %d\n',norm(w));
+    disp(['%%%%%%%%%%%%%% MU = ' num2str(mu(i))]); 
+    disp(['Optimal Objective: ' num2str(objective)]);
+    disp(['Gamma: ' num2str(gam)]);
+    disp(['||w|| = ' num2str(norm(w))]);
     fprintf('Train Correct Classification: %d\n',correct_Train);
     fprintf('Train Wrong Classification: %d\n\n',wrong_Train);
     fprintf('Tune Correct Classification: %d\n',correct_Tune);
--- question3_4.m
+++ question3_4.m
@@ -5,5 +5,12 @@ mu = .1;
 
 [feats,w,gam,numWrong] = bestFeatures(train,tune,features,mu);
 fprintf('\n');
-fprintf('Optimal Classifier: w = %d\tgamma = %d\n',w,gam);
+fprintf('Optimal Classifier:\n');
+disp('Optimal w = ');
+disp(w);
+disp('Optimal gamma = ');
+disp(gam);
 fprintf('Words Used: %s\t%s\n',wordlist{feats(1)},wordlist{feats(2)});
+
+disp('Disputed Papers: ');
+predictAuthorship(w,gam,test,feats);
